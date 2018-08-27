fcee780728bc8785a9e8037756b6f904f6d03aa9
Jonathan DeMarks <jdemarks@geniuscentral.com>
2018-08-27 10:20:37 -0400

    Refactor common operations

--- SendOrderAcknowledgements.ps1
+++ SendOrderAcknowledgements.ps1
@@ -1,65 +1,12 @@
 Param(
-  [bool]$test = $false
+  [bool]$test = $False
 )
 
 Clear-Host
 
-# Read in configuration file
-$config = Get-Content -Raw -Path "config.json" | ConvertFrom-Json
-
-# Create folders if they do not exist
-If(!(Test-Path $config.logFile)) { New-Item $config.logFile -type directory }
-If(!(Test-Path $config.acknowledgementsJsonPath)) { New-Item $config.acknowledgementsJsonPath -type directory }
-If(!(Test-Path $config.acknowledgementsHistoryPath)) { New-Item $config.acknowledgementsHistoryPath -type directory }
-If(!(Test-Path $config.acknowledgementsFailedPath)) { New-Item $config.acknowledgementsFailedPath -type directory }
-
-# Simplified logging
-function Add-LogEntry([string]$message) {
-    Add-Content $logFile $message
-    if ($config.outputToConsole) { Write-Host $message }
-}
-
-# Open log
-$logFile = "$($config.logFile)SendOrderAcknowledgementsLog_$(Get-Date -Format ""yyyy-MM-dd"").txt"
-Add-LogEntry "Start of send order acknowledgements script - $(Get-Date -Format ""u"")"
-
-# Read in order acknowledgements
-try {
-    $accessToken = .\scripts\GetAccessToken.ps1 $config.clientSecret $config.identityAPIUri  $config.clientId $logFile $config.debug
-    if ($accessToken) {
-        # Configure API access
-        $headers = @{"Authorization" = "Bearer $accessToken"; "Accept" = "application/json"; "Content-Type" = "application/json"}
-
-        # Iterate over each file
-        Get-ChildItem -File $config.acknowledgementsJsonPath | ForEach-Object {
-            $file = $_;
-            Add-LogEntry "Processing file $($file.FullName)..."
-            try {
-                $body = Get-Content -Raw -Path $file.FullName
-                $data = $body | ConvertFrom-Json
-                $url = "$($config.posAPIUri)edi/orders/$($data.orderId)/acknowledgements"
-
-                $result = Invoke-RestMethod -Method Post -Uri $url -Body $body -Headers $headers
-                if ($config.debug) { Add-LogEntry "Got result: $(ConvertTo-Json $result)" }
-
-                if ($test -eq $false) {
-                    $destination = [System.IO.Path]::Combine($config.acknowledgementsHistoryPath, $file.Name)
-                    Move-Item -Force -Path $file.FullName -Destination $destination
-                    Add-LogEntry "Moved $($file.Name) to $destination"
-                }
-            }
-            catch {
-                $destination = [System.IO.Path]::Combine($config.acknowledgementsFailedPath, $file.Name)
-                Move-Item -Force -Path $file.FullName -Destination $destination
-                $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
-                $errResp = $streamReader.ReadToEnd() | ConvertFrom-Json
-                $streamReader.Close()
-                Add-LogEntry "Failed to send order acknowledgement. $($_.Exception.Message) $errResp"
-            }
-        }
-    }
-}
-catch {
-    Add-LogEntry "Send order acknowledgements error on line $($_.Exception.ScriptLineNumber): $($_.Exception.Message)"
-    throw $_.Exception
-}
+.\scripts\SendDocumentsToEdiApi.ps1 `
+    -configPrefix "acknowledgements" `
+    -ediApiEndpoint "acknowledgements" `
+    -logPrefix "SendOrderAcknowledgementsLog_" `
+    -readableName "send order acknowledgements" `
+    -test $test
--- SendShippingNotices.ps1
+++ SendShippingNotices.ps1
@@ -1,65 +1,12 @@
 Param(
-  [bool]$test = $false
+  [bool]$test = $False
 )
 
 Clear-Host
 
-# Read in configuration file
-$config = Get-Content -Raw -Path "config.json" | ConvertFrom-Json
-
-# Create folders if they do not exist
-If(!(Test-Path $config.logFile)) { New-Item $config.logFile -type directory }
-If(!(Test-Path $config.shippingNoticeJsonPath)) { New-Item $config.shippingNoticeJsonPath -type directory }
-If(!(Test-Path $config.shippingNoticeHistoryPath)) { New-Item $config.shippingNoticeHistoryPath -type directory }
-If(!(Test-Path $config.shippingNoticeFailedPath)) { New-Item $config.shippingNoticeFailedPath -type directory }
-
-# Simplified logging
-function Add-LogEntry([string]$message) {
-    Add-Content $logFile $message
-    if ($config.outputToConsole) { Write-Host $message }
-}
-
-# Open log
-$logFile = "$($config.logFile)SendShippingNoticesLog_$(Get-Date -Format ""yyyy-MM-dd"").txt"
-Add-LogEntry "Start of send shipping notices script - $(Get-Date -Format ""u"")"
-
-# Read in shipping notices
-try {
-    $accessToken = .\scripts\GetAccessToken.ps1 $config.clientSecret $config.identityAPIUri  $config.clientId $logFile $config.debug
-    if ($accessToken) {
-        # Configure API access
-        $headers = @{"Authorization" = "Bearer $accessToken"; "Accept" = "application/json"; "Content-Type" = "application/json"}
-
-        # Iterate over each file
-        Get-ChildItem -File $config.shippingNoticeJsonPath | ForEach-Object {
-            $file = $_;
-            Add-LogEntry "Processing file $($file.FullName)..."
-            try {
-                $body = Get-Content -Raw -Path $file.FullName
-                $data = $body | ConvertFrom-Json
-                $url = "$($config.posAPIUri)edi/orders/$($data.orderId)/shipping-notices"
-
-                $result = Invoke-RestMethod -Method Post -Uri $url -Body $body -Headers $headers
-                if ($config.debug) { Add-LogEntry "Got result: $(ConvertTo-Json $result)" }
-
-                if ($test -eq $false) {
-                    $destination = [System.IO.Path]::Combine($config.shippingNoticeHistoryPath, $file.Name)
-                    Move-Item -Force -Path $file.FullName -Destination $destination
-                    Add-LogEntry "Moved $($file.Name) to $destination"
-                }
-            }
-            catch {
-                $destination = [System.IO.Path]::Combine($config.shippingNoticeFailedPath, $file.Name)
-                Move-Item -Force -Path $file.FullName -Destination $destination
-                $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
-                $errResp = $streamReader.ReadToEnd() | ConvertFrom-Json
-                $streamReader.Close()
-                Add-LogEntry "Failed to send shipping notice. $($_.Exception.Message)"
-            }
-        }
-    }
-}
-catch {
-    Add-LogEntry "Send shipping notices error on line $($_.Exception.ScriptLineNumber): $($_.Exception.Message)"
-    throw $_.Exception
-}
+.\scripts\SendDocumentsToEdiApi.ps1 `
+    -configPrefix "shippingNotice" `
+    -ediApiEndpoint "shipping-notices" `
+    -logPrefix "SendShippingNoticesLog_" `
+    -readableName "send shipping notices" `
+    -test $test
